2d78fac4d669fa77098420250d73c4641ed143b6
v.babin <v.babin@bossgs.org>
2019-04-17 11:20:45 +0300

    started created sidebar

--- wp-content/themes/sparrow/functions.php
+++ wp-content/themes/sparrow/functions.php
@@ -5,15 +5,19 @@ add_action('wp_footer', 'scripts_theme');
 add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
 add_action('after_setup_theme', 'my_menu');
 
-function my_menu() {
-	register_nav_menu('top', 'меню в шапке');
-}
-
-
+/*
+ * register jQuery (with CDN)
+*/
 
 function my_scripts_method(){
+	wp_deregister_script( 'jquery' );
+	wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js', true );
 	wp_enqueue_script( 'jquery' );
 }
+
+/*
+ * register css style
+*/
 
 function style_theme() {
 	wp_enqueue_style('style', get_stylesheet_uri());
@@ -22,10 +26,24 @@ function style_theme() {
 	wp_enqueue_style('media-queries', get_template_directory_uri().'/assets/css/media-queries.css');
 }
 
+/*
+ * register js scripts
+*/
+
 function scripts_theme() {
 	wp_enqueue_script('init', get_template_directory_uri() . '/assets/js/init.js');
 	wp_enqueue_script('doubletaptogo', get_template_directory_uri().'/assets/js/doubletaptogo.js');
 	wp_enqueue_script('flexslider', get_template_directory_uri().'/assets/js/jquery.flexslider.js');
 	wp_enqueue_script('modernizr', get_template_directory_uri().'/assets/js/modernizr.js');
+}
+
+
+/*
+ * register menu
+*/
+
+function my_menu() {
+	register_nav_menu('top', 'меню в шапке');
+	register_nav_menu('footer', 'меню в footer');
 }
 
